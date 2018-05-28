046cc88525a721d8223a490021cf30fcfee4b74b
Darek <darek.charkot@gmail.com>
2018-05-28 22:30:36 +0200

    Add static-pages controller with views. Create routes to hardwares and home page. Create menu bar. Make few beautifies

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def services
+  end
+
+  def pricelist
+  end
+
+  def technology
+  end
+
+  def hardwares
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'pages/hardwares', to: 'hardwares#index'
+
+  get 'pages/home'
+
+  get 'pages/services'
+
+  get 'pages/pricelist'
+
+  get 'pages/technology'
+
+  get 'pages/contact'
+
   resources :hardwares
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
-  root "hardwares#index"
+  root "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
