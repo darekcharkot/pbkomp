b37ea3c43634d4f53780925bc7716eea718a93f2
Darek <darek.charkot@gmail.com>
2018-07-31 11:42:17 +0200

    Add Nick field to User model. Change invite after login to display nick name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
 	protected
@@ -9,4 +10,8 @@ class ApplicationController < ActionController::Base
 		def after_sign_out_path_for(resource)
 			pages_contact_path
 		end
+
+		def configure_permitted_parameters
+    	devise_parameter_sanitizer.permit(:sign_up, keys: [:nick])
+  	end
 end
