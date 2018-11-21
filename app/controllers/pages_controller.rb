d59bcb7168e6a2a23a28fe88984c45f324505f31
Darek <darek.charkot@gmail.com>
2018-11-21 12:35:52 +0100

    Add offer menu page for clients

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   def home
   end
 
+  def offer
+    @hardwares = Hardware.all.order(:created_at)
+  end
+
   def services
   end
 
@@ -15,5 +19,13 @@ class PagesController < ApplicationController
   end
 
   def contact
+  end
+
+  private
+
+  def hardware_params
+    params.require(:hardware).permit(:name, :model, :sn, :foreign_code, :own_code, :buy_brutto, :proof,
+                   :buy_date, :sold, :sell_date, :created_at, :describe, :processor, :ram, :disk, :screen, :dvd,
+                   :video_card, :battery, :operating_system, :warranty)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/hardwares', to: 'hardwares#index'
 
+  get 'pages/offer'
+
   get 'pages/home'
 
   get 'pages/services'
