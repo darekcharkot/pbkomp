c19fc0b2b5a921a04711e7e9b48d1b2c2545e1be
Darek <darek.charkot@gmail.com>
2018-12-01 14:53:53 +0100

    Pull changes from heroku and migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 20181121113828) do
   enable_extension "plpgsql"
 
   create_table "hardwares", force: :cascade do |t|
-    t.string "name"
     t.string "model"
     t.string "sn"
     t.text "describe"
@@ -37,8 +36,11 @@ ActiveRecord::Schema.define(version: 20181121113828) do
     t.string "warranty"
     t.string "proof"
     t.datetime "buy_date"
+    t.bigint "type_hardware_id"
+    t.string "name"
     t.datetime "sell_date"
     t.string "price"
+    t.index ["type_hardware_id"], name: "index_hardwares_on_type_hardware_id"
   end
 
   create_table "users", force: :cascade do |t|
