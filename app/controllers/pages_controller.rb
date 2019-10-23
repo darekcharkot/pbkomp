cb2ea30505acdec8e17b389d44455ae613fd42ab
Darek <darek.charkot@gmail.com>
2019-10-23 12:41:44 +0200

    Repair displaying unsolded hardwares. Change view in hardwares path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -34,10 +34,4 @@ class PagesController < ApplicationController
     def set_hardware
       @hardware = Hardware.find(params[:id])
     end
-
-    def hardware_params
-        params.require(:hardware).permit(:name, :model, :sn, :foreign_code, :own_code, :buy_brutto, :proof,
-                       :buy_date, :sold, :sell_date, :created_at, :describe, :processor, :ram, :disk, :screen, :dvd,
-                       :video_card, :battery, :operating_system, :warranty, :price, images_attributes: [:image, :hardware_id])
-    end
 end
